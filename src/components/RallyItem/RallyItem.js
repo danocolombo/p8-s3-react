4d8c2f92b2b374ca62da173d57738c1a19a23737
danocolombo@google.com <GR0mans1212!>
2019-12-07 15:43:52 -0500

    continued work on trying to get registration form

--- src/components/RallyItem/RallyItem.js
+++ src/components/RallyItem/RallyItem.js
@@ -3,8 +3,11 @@ import { withRouter } from 'react-router-dom';
 import './RallyItem.css';
 
 const rallyitem = (props) => (
+    
     <section>
-        <div className="EventDate">{props.eventDate}</div>
+        { target = 'test'}
+        <div>ID: {props.eventID}</div>
+        <div className="EventDate"><a href="{{this.eventID}}">{props.eventDate}</a></div>
         <div className="ChurchName">{props.locationName}</div>
         <div className="ChurchLocation">{props.locationStreet}<br/>
         {props.locationCity},{props.locationState} {props.locationZipcode}</div>
