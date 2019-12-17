ea05f5d6980dfdb4fc31459b15adefeb1b4a9fbd
danocolombo@google.com <GR0mans1212!>
2019-12-17 17:44:55 -0500

    added hamburger image

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Toolbar.css';
+import DrawerToogleButton from '../../SideDrawer/DrawerToggleButton';
 const Toolbar = props => (
     <header className="toolbar">
         <nav className='toolbar_navigation'>
-            <div></div>
+            <div>
+                <DrawerToogleButton/>
+            </div>
             <div className="toolbar_logo"> <a href="/">THE LOGO</a> </div>
             <div className="spacer"/>
             <div className="toolbar_navigation-items">
