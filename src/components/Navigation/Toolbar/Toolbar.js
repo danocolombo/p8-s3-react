e26b66546166ca9ab666590a16b4505d6e5c0c74
danocolombo@google.com <GR0mans1212!>
2019-12-17 06:20:27 -0500

    navigation bar cleaned up

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import classes from './Toolbar.css';
-const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <nav></nav>
+import React from 'react'
+import './Toolbar.css';
+const Toolbar = props => (
+    <header className="toolbar">
+        <nav className='toolbar_navigation'>
+            <div></div>
+            <div className="toolbar_logo"> <a href="/">THE LOGO</a> </div>
+            <div className="spacer"/>
+            <div className="toolbar_navigation-items">
+                <ul>
+                    <li><a href="/">Home</a></li> 
+                    <li><a href="/rallies">Events</a></li>  
+                </ul>                
+            </div>
+        </nav>
     </header>
-
-);
-export default toolbar;
+)
+export default Toolbar;
--- src/containers/P8/P8.js
+++ src/containers/P8/P8.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 // import axios from '../../axios';
-import Home from '../../components/Home/Home';
-//import Posts from './Posts/Posts';
-import Rallies from '../../components/Rallies/Rallies';
 import Register from '../../containers/Register/Register';
 import { Route, Link } from 'react-router-dom';
-import Layout from '../../hoc/Layout/Layout';
+// import Layout from '../../hoc/Layout/Layout';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import Home from '../../components/Home/Home';
+import Rallies from '../../components/Rallies/Rallies';
+// import MenuBar from '../../components/Navigation/Toolbar/MenuBar';
+// import Menu from '@material-ui/core/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
 import './P8.css';
 //==========================================================
 // this is used for the P8 Rally Centrol base functionality
@@ -15,12 +17,11 @@ class P8 extends Component {
     render () {
         return (
             <div>
-                <Layout>
-                <Toolbar/>
+                    <Toolbar/>
                     <Route path="/" exact component={Home} />
                     <Route path="/rallies" component={Rallies} />
                     <Route path="/register" component={Register} />
-                </Layout>
+                
             </div>
         );
     }
