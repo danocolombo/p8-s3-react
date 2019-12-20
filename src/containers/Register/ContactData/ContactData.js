3751952596b93e281991c1eed64f27b1f9b95e17
danocolombo <danocolombo@gmail.com>
2019-12-19 22:43:43 -0500

    layout adjustments

--- src/containers/Register/ContactData/ContactData.js
+++ src/containers/Register/ContactData/ContactData.js
@@ -5,8 +5,8 @@ class ContactData extends Component {
     render() {
         return (
             <div className="parent">
-                <div className="fName"><p className="labels">First Name</p></div>
-                <div><p className="inputField"><input type="text" /></p></div>
+                <div className="fName"><p className="labels">First Name</p>
+                <br/><input className="inputField" type="text" /></div>
                 <div className="lName"><p className="labels">Last Name</p><p><input type="text" /></p></div>
                 <div className="phone"><p className="labels">Phone</p><p><input type="text" /></p></div>
                 <div className="email"><p className="labels">Email</p><p><input type="text" /></p></div>
