1a9d0260eb3a5a0a171d8b2b0f81d483404f8ad3
danocolombo <danocolombo@gmail.com>
2019-12-21 13:38:54 -0500

    touching up rallies formatting

--- src/components/RallyItem/RallyItem.js
+++ src/components/RallyItem/RallyItem.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 //import Button from '../UI/Button/Button';
 import Button from '@material-ui/core/Button';
-import classes from './RallyItem.css';
+import './RallyItem.css';
 
 const rallyitem = (props) => {
     return (
     <section>
-        
-        <div className={classes.RallyItem}>{props.eventDate}
+        <div className='RallyBox'>
+        <div className='EventDate'>{props.eventDate}
         <button className="registerButton" onClick={props.clicked}>REGISTER</button></div>
         <div className="ChurchName" onClick={props.clicked}>{props.locationName}
         </div>
         <div className="ChurchLocation">{props.locationStreet}<br/>
         {props.locationCity},{props.locationState} {props.locationZipcode}</div>
-        <br/>
+        </div>
     </section>
     );
 }
