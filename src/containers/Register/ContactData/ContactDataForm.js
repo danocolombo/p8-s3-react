8c3f02b10add8ba31f8abdde93dca3a6cbf3b624
danocolombo@google.com <GR0mans1212!>
2020-01-10 06:38:39 -0500

    added materialize-ui/icons and new fields to register form

--- src/containers/Register/ContactData/ContactDataForm.js
+++ src/containers/Register/ContactData/ContactDataForm.js
@@ -1,188 +1,260 @@
 import React from "react";
+//------- materialize imports/configs START
+import classNames from "classnames";
+// material-ui components
+import { makeStyles } from "@material-ui/core/styles";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+// @material-ui/icons
+import Check from "@material-ui/icons/Check";
+
+//import styles from "assets/jss/material-kit-react/customCheckboxRadioSwitch.js";
+import styles from "../../../assets/jss/material-kit-pro-react/customCheckboxRadioSwitchStyle";
+const useStyles = makeStyles(styles);
+//------- materialize imports/configs END
 // import './ContactData.css'
 function RegisterForm(props) {
-  return (
-    <form>
-      <div>
-        <label className="RegisterLabel" htmlFor="title">Your name</label>
-        <div>
-          <input
-            id="firstName"
-            type="text"
-            onChange={props.onFieldChange}
-            name="fName"
-            className="RegisterControl"
-            placeholder="first name"
-            value={props.registration.fName}
-          />
-             
-          <input
-            id="lastName"
-            type="text"
-            onChange={props.onFieldChange}
-            name="lName"
-            className="RegisterControl"
-            placeholder="last name"
-            value={props.registration.lName}
-            
-          />
-        </div>
-      </div>
+    const [checked, setChecked] = React.useState([24, 22]);
+    const classes = useStyles();
+    const wrapperDiv = classNames(
+        classes.checkboxAndRadio,
+        classes.checkboxAndRadioHorizontal
+    );
+    const handleToggle = value => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
 
-      <div>
-        <label className="RegisterLabel" htmlFor="email">Email</label>
-        <div>
-        <input
-            id="email"
-            type="text"
-            onChange={props.onFieldChange}
-            name="email"
-            className="RegisterControl"
-            placeholder="email@address.here"
-            value={props.registration.email}
-            size="50"
-          />
-        </div>
-      </div>
-      <div>
-      <label className="RegisterLabel" htmlFor="phone">Phone Number</label>
-        <div>
-          <input
-            id="phone"
-            type="text"
-            onChange={props.onFieldChange}
-            name="phone"
-            className="RegisterControl"
-            placeholder="(123) 456-1234"
-            value={props.registration.phone}
-          />
-        </div>
-      </div>
-      <div>
-        <div>
-          <input
-            type="checkbox"
-            onChange={props.onFieldChange}
-            id="newsletter"
-            name="newsletter"
-            className="RegisterControl"
-            value={props.registration.newsletter}
-          />
-          <label className="NewsletterLabel" htmlFor="category">&nbsp;&nbsp;Would you like to receive the national CR newsletter?</label>
-        </div>
-      </div>
-      <div>
-        
-        <div>
-          <select
-            id="registrationCnt"
-            name="registrationCount"
-            onChange={props.onFieldChange}
-            value={props.registration.registrationCount}
-            className="RegistrationControl"
-          >
-            <option value="" />
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-          <label className="RegCountLabel" htmlFor="email">&nbsp;&nbsp;Counting yourself, how many are you registering for the event?</label>
-        </div>
-      </div>
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+        setChecked(newChecked);
+    };
 
+    return (
+        <form>
+            <div>
+                <label className='RegisterLabel' htmlFor='title'>
+                    Your name
+                </label>
+                <div>
+                    <input
+                        id='firstName'
+                        type='text'
+                        onChange={props.onFieldChange}
+                        name='fName'
+                        className='RegisterControl'
+                        placeholder='first name'
+                        value={props.registration.fName}
+                    />
 
-      <div>
-        <label className="RegisterLabel" htmlFor="crName">Your CR Church</label>
-        <div>
-          <input
-            type="text"
-            id="category"
-            name="crName"
-            placeholder="Church Name"
-            onChange={props.onFieldChange}
-            className="RegisterControl"
-            value={props.registration.crName}
-          />
-        </div>
-      </div>
-      <div>
-        <label className="RegisterLabel" htmlFor="crCity">Your CR Location</label>
-        <div>
-          <input
-            type="text"
-            id="crCity"
-            name="crCity"
-            placeholder="City"
-            onChange={props.onFieldChange}
-            className="RegisterControl"
-            value={props.registration.crCity}
-          />
-          <input
-            type="text"
-            id="crState"
-            name="crState"
-            placeholder="State"
-            onChange={props.onFieldChange}
-            className="RegisterControl"
-            value={props.registration.crState}
-          />
-        </div>
-      </div>
-      <div>
-        <label className="RegisterLabel" htmlFor="email">What role do you have at your CR?</label>
-        <div>
-          <select
-            id="crRole"
-            name="crRole"
-            onChange={props.onFieldChange}
-            value={props.registration.crRole}
-            className="RegisterControl"
-          >
-            <option value="1">Ministry Leader</option>
-            <option value="2">Leader</option>
-            <option value="3">Volunteer</option>
-            <option value="4">Participant</option>
-          </select>
-        </div>
-      </div>
-      <div>
-        <div>
-          <input
-            type="checkbox"
-            onChange={props.onFieldChange}
-            id="hostP8"
-            name="hostP8"
-            className="RegisterControl"
-            value={props.registration.newsletter}
-          />
-          <label className="HostP8Label" htmlFor="hostP8">&nbsp;&nbsp;Would your CR be willing to host a future P8 rally?</label>
-        </div>
-      </div>
-      <div>
-        <label className="RegisterLabel" htmlFor="notes">Comments</label>
-        <div>
-          <input
-            type="textbox"
-            id="notes"
-            name="notes"
-            row="4"
-            onChange={props.onFieldChange}
-            className="RegisterControl"
-            value={props.registration.notes}
-          />
-        </div>
-      </div>
-      <div></div>
-      <input type="submit" value="Save" className="btn btn-primary" />
-    </form>
-  );
+                    <input
+                        id='lastName'
+                        type='text'
+                        onChange={props.onFieldChange}
+                        name='lName'
+                        className='RegisterControl'
+                        placeholder='last name'
+                        value={props.registration.lName}
+                    />
+                </div>
+            </div>
+
+            <div>
+                <br />
+                <label className='RegisterLabel' htmlFor='email'>
+                    Email
+                </label>
+                <div>
+                    <input
+                        id='email'
+                        type='text'
+                        onChange={props.onFieldChange}
+                        name='email'
+                        className='RegisterControl'
+                        placeholder='email@address.here'
+                        value={props.registration.email}
+                        size='50'
+                    />
+                </div>
+            </div>
+            <div>
+                <br />
+                <label className='RegisterLabel' htmlFor='phone'>
+                    Phone Number
+                </label>
+                <div>
+                    <input
+                        id='phone'
+                        type='text'
+                        onChange={props.onFieldChange}
+                        name='phone'
+                        className='RegisterControl'
+                        placeholder='(123) 456-1234'
+                        value={props.registration.phone}
+                    />
+                </div>
+            </div>
+            <div className={wrapperDiv}>
+                <br />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            tabIndex={-1}
+                            onClick={() => handleToggle(21)}
+                            checkedIcon={
+                                <Check className={classes.checkedIcon} />
+                            }
+                            icon={<Check className={classes.uncheckedIcon} />}
+                            classes={{ checked: classes.checked }}
+                        />
+                    }
+                    classes={{ label: classes.label }}
+                    label='Would you like to receive the national CR
+                    newsletter?'
+                />
+            </div>
+            {/* <div>
+                <div>
+                    <input
+                        type='checkbox'
+                        onChange={props.onFieldChange}
+                        id='newsletter'
+                        name='newsletter'
+                        className='RegisterControl'
+                        value={props.registration.newsletter}
+                    />
+                    <label className='NewsletterLabel' htmlFor='category'>
+                        &nbsp;&nbsp;Would you like to receive the national CR
+                        newsletter?
+                    </label>
+                </div>
+            </div> */}
+            <div>
+                <div>
+                    <select
+                        id='registrationCnt'
+                        name='registrationCount'
+                        onChange={props.onFieldChange}
+                        value={props.registration.registrationCount}
+                        className='RegistrationControl'
+                    >
+                        <option value='' />
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                        <option value='6'>6</option>
+                        <option value='7'>7</option>
+                        <option value='8'>8</option>
+                        <option value='9'>9</option>
+                        <option value='10'>10</option>
+                    </select>
+                    <label className='RegCountLabel' htmlFor='email'>
+                        &nbsp;&nbsp;Counting yourself, how many are you
+                        registering for the event?
+                    </label>
+                </div>
+            </div>
+
+            <div>
+                <label className='RegisterLabel' htmlFor='crName'>
+                    Your CR Church
+                </label>
+                <div>
+                    <input
+                        type='text'
+                        id='category'
+                        name='crName'
+                        placeholder='Church Name'
+                        onChange={props.onFieldChange}
+                        className='RegisterControl'
+                        value={props.registration.crName}
+                    />
+                </div>
+            </div>
+            <div>
+                <label className='RegisterLabel' htmlFor='crCity'>
+                    Your CR Location
+                </label>
+                <div>
+                    <input
+                        type='text'
+                        id='crCity'
+                        name='crCity'
+                        placeholder='City'
+                        onChange={props.onFieldChange}
+                        className='RegisterControl'
+                        value={props.registration.crCity}
+                    />
+                    <input
+                        type='text'
+                        id='crState'
+                        name='crState'
+                        placeholder='State'
+                        onChange={props.onFieldChange}
+                        className='RegisterControl'
+                        value={props.registration.crState}
+                    />
+                </div>
+            </div>
+            <div>
+                <label className='RegisterLabel' htmlFor='email'>
+                    What role do you have at your CR?
+                </label>
+                <div>
+                    <select
+                        id='crRole'
+                        name='crRole'
+                        onChange={props.onFieldChange}
+                        value={props.registration.crRole}
+                        className='RegisterControl'
+                    >
+                        <option value='1'>Ministry Leader</option>
+                        <option value='2'>Leader</option>
+                        <option value='3'>Volunteer</option>
+                        <option value='4'>Participant</option>
+                    </select>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <input
+                        type='checkbox'
+                        onChange={props.onFieldChange}
+                        id='hostP8'
+                        name='hostP8'
+                        className='RegisterControl'
+                        value={props.registration.newsletter}
+                    />
+                    <label className='HostP8Label' htmlFor='hostP8'>
+                        &nbsp;&nbsp;Would your CR be willing to host a future P8
+                        rally?
+                    </label>
+                </div>
+            </div>
+            <div>
+                <label className='RegisterLabel' htmlFor='notes'>
+                    Comments
+                </label>
+                <div>
+                    <input
+                        type='textbox'
+                        id='notes'
+                        name='notes'
+                        row='4'
+                        onChange={props.onFieldChange}
+                        className='RegisterControl'
+                        value={props.registration.notes}
+                    />
+                </div>
+            </div>
+            <div></div>
+            <input type='submit' value='Save' className='btn btn-primary' />
+        </form>
+    );
 }
 
 export default RegisterForm;
